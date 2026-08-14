f2560fb0c746c144019ca0e0a22019da976480c4
ku-134 <f2010211@163.com>
2026-08-14 14:35:15 +0800

    Collision v2: register 5 new classes

--- Gamecurrently/Collision/js/skills/skillRegistry.js
+++ Gamecurrently/Collision/js/skills/skillRegistry.js
@@ -1,8 +1,13 @@
 import { SkillInstance } from './skillSystem.js';
 import giant from './definitions/giant.js';
 import legion from './definitions/legion.js';
+import poison from './definitions/poison.js';
+import thorn from './definitions/thorn.js';
+import magnet from './definitions/magnet.js';
+import puppet from './definitions/puppet.js';
+import phantom from './definitions/phantom.js';
 
-const defs = [giant, legion];
+const defs = [giant, legion, poison, thorn, magnet, puppet, phantom];
 const byId = Object.fromEntries(defs.map(d => [d.id, d]));
 
 export const getSkillDefs = () => defs;
